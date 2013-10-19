3e60d0430f59b0266d570de957796f37f99be0a9
James-Adam Renquinha Henri <jarhmander@gmail.com>
2013-10-19 18:30:27 -0400

    Begin of option handling code.

--- main.c
+++ main.c
@@ -1,12 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "lualib.h"
 #include "lauxlib.h"
 #include "lualib.h"
 
 extern int luaopen_lpeg (lua_State *L);
 
-int main()
+static int short_opt(int c, char const *rest)
+{
+}
+
+static int long_opt(char const *str, size_t len, char const *arg)
+{
+
+}
+
+static int not_opt(char const *str)
+{
+
+}
+
+static int handle_options(int argc, char const *argv[])
+{
+    int i;
+    int endcollect = 0;
+    int code = 0;
+
+    for (i=1; i < argc && code != -1; ++i)
+    {
+        char const *opt = argv[i];
+        if (opt[0] == '-')
+        {
+            if (opt[1] == '-' && !endcollect)
+            {
+                // Collect potential options
+                if (opt[2] == 0)
+                {
+                    // End of options
+                    endcollect = 1;
+                }
+                else
+                {
+                    // Long opt
+                    size_t loc = strcspn(opt, "=");
+                    if (opt[loc])
+                    {
+                        // char '=' found.
+                        code = long_opt(opt+2, loc-2, opt+loc+1);
+                    }
+                    else
+                    {
+                        code = long_opt(opt+2, loc-2, argv[i+1]);
+                        if (code == 1)
+                        {
+                            // long_opt has consumed the arg
+                            ++i;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                // short opt
+                int j;
+                for (j=2; opt[j]; ++j)
+                {
+                    code = short_opt(opt[j], opt+j+1);
+                    // Break either if arg is consumed or there is an error.
+                    if (code) break;
+                }
+            }
+        }
+            // Collect 'floating' (positional) arguments
+            else // ==> (endcollect || opt[1] != '-')
+            {
+                code = not_opt(opt);
+            }
+
+    }
+}
+
+int main(int argc, char const *argv[])
 {
     lua_State *L = luaL_newstate();
     if (!L)
